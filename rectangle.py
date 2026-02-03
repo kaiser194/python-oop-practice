76756c36f5b019a34429a9e504fe90bdb1574bfd
kaiser-dev <saivignan08@gmail.com>
2026-02-03 21:20:35 +0530

    Update rectangle.py

--- rectangle.py
+++ rectangle.py
@@ -1,1 +1,21 @@
-
+class Rectangle:
+    
+    def __init__(self,l,w):
+        self.length = l
+        self.width = w 
+        
+        
+    def perimeter(self):
+        return 2*(self.length+self.width)
+        
+    def area(self):
+        return self.length*self.width
+        
+    def display(self):
+      print('The length of rectangle is: ',self.length)
+      print('The width of rectangle is: ',self.width)
+      print('The perimeter of rectangle is: ',self.perimeter())
+      print('The area of rectangle is: ',self.area())
+        
+obj = Rectangle(3,4)
+obj.display()
